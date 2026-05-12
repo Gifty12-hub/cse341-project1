8b0dda60d452c686a0a994364bc6c94dfd2a5458
Giftyarkoh <garkoh@byupathway.edu>
2026-05-12 00:15:18 +0000

    in with my project 1 lesson 1 added the get and get all endpoints

---
+++ controllers/users.js
@@ -0,0 +1,24 @@
+const mongodb = require('../data/database');
+const ObjectId = require('mongodb').ObjectId;
+
+const getAll = async (req, res) => {
+    const results = await mongodb.getDatabase().db().collection('users').find();
+    results.toArray().then((users) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(users);
+    });
+};
+
+const getSingle = async (req, res) => {
+    const userId = req.params.id;
+    const results = await mongodb.getDatabase().db().collection('users').find({_id: userId});
+    results.toArray().then((users) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(users[0]);
+    });  
+};
+
+module.exports = {
+    getAll,
+    getSingle
+};
